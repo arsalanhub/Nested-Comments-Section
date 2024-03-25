1f13f4625cbe6c02912ac885123deb7dda452e7a
marsalan <marsalan@hhaexhange.com>
2024-03-25 14:51:48 +0530

    nested comments

--- src/Components/Comments/index.jsx
+++ src/Components/Comments/index.jsx
@@ -2,21 +2,24 @@ import styles from "./styles.module.css"
 
 const CommentItem = ({ comment }) => {
     return (
-        <div className={styles.commentContainer}>
-            <div>{comment.comment}</div>
-            <div className={styles.controls}>
-                <span>View Reply</span>
-                <span>Add Reply</span>
+        <>
+            <div className={styles.commentContainer}>
+                <div>{comment.comment}</div>
+                <div className={styles.controls}>
+                    <span>View Reply</span>
+                    <span>Add Reply</span>
+                </div>
+                {comment.subComments.length>0 && <Comment commentsData={comment.subComments} />}
             </div>
-        </div>
+        </>
     )
 }
 const Comment = ({ commentsData }) => {
     console.log(commentsData)
     return (
         <>
-            {commentsData.map((comment) => 
-               <CommentItem comment={comment} key={comment.id} />
+            {commentsData.map((comment) =>
+                <CommentItem comment={comment} key={comment.id} />
             )}
         </>
     )
